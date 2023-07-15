5d37e5677d250d189d2dd9e559a9f15ea35ff1c6
jessianne <jessianne_torres@hotmail.com>
2023-07-15 16:01:56 -0300

    primeiro commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const dia = document.getElementById('dia');
+const noite = document.getElementById('noite');
+
+dia.addEventListener ("click", function(){
+    document.body.style.backgroundImage = 'url("https://i.pinimg.com/564x/35/d3/5b/35d35bab50560703577a10f27f2f6785.jpg")';
+});
+
+noite.addEventListener ("click", function(){
+    document.body.style.backgroundImage = 'url("https://i.pinimg.com/564x/d8/37/ef/d837ef3b34a12a8d55ab8e64e4c38a04.jpg")';
+});
